fc2cdb6a13805b41a56c53efe9795bc028da3dc3
ouhoy <kimkinon@gmail.com>
2023-03-09 10:58:25 +0100

    Do Java Basics

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
 public class Main {
     public static void main(String[] args) {
 
-        String myName = "Abdallah";
+        final String myName = "Abdallah";
+        final int myAge = 23;
 
-        System.out.println(myName);
+        //TODO Print something
+        System.out.println(myName + " " + myAge);
+
+        if (myAge >= 18) {
+            System.out.println("You are allowed to drive!!");
+        } else {
+            System.out.println("You are not allowed to drive!");
+        }
+
+        int month = 3;
+        String monthString;
+
+        // TODO Try Switch Cases in Java
+        switch (month) {
+            case 1:
+                monthString = "January";
+                System.out.println(monthString);
+                break;
+            default:
+                break;
+        }
+        // TODO Try For loop
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Day " + i);
+        }
+
+        // TODO Try While Loop
+        int count = 1;
+        while (count < 10) {
+
+            System.out.println(count);
+            count++;
+        }
 
     }
 }
