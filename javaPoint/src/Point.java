27c44bace45ed30b60325779ac2aafb1e90210d4
ouhoy <kimkinon@gmail.com>
2023-11-07 21:13:49 +0100

    Add java point starting file

---
+++ javaPoint/src/Point.java
@@ -0,0 +1,19 @@
+public class Point {
+
+    double x = Math.random() * 100;
+    double y = Math.random() * 100;
+    double z = Math.random() * 100;
+    public Point(){}
+
+    public String toString() {
+        return "X: " + x + " Y : "+ y + " Z: " + z;
+    }
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 300; i ++) {
+
+        }
+
+    }
+}
